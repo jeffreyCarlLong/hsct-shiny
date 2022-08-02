ae438a985c7d1bd00e11dd782a5516741afe4120
Jeffrey C. Long <jeffrey.c.long@gmail.com>
2022-08-02 06:49:50 -0700

    Updating data dictionary

--- app.R
+++ app.R
@@ -79,7 +79,7 @@ cytomegalovirus$cgvhd <- as.factor(cytomegalovirus$cgvhd)
 
 
 
-dict <- read.delim("~/r/hsct-shiny/CytomegalovirusDataDictionary.tsv", sep = "\t")
+dict <- read.delim("~/r/hsct-shiny/hsct_data_dictionary.tsv", sep = "\t")
 
 
 # Define UI for application that draws a histogram
